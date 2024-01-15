7e64449525040d6df29b640c7eed7c232e5a522e
T-H9703EnAc <t.h.1997.m.engineer.ac@gmail.com>
2024-01-15 23:44:35 +0900

    first commit

---
+++ app/pages/hello.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>Hello Page</h1>
+    </div>
+</template>
+<script>
+export default {
+  // スクリプトの内容
+};
+</script>
---
+++ app/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>TOP Page</h1>
+    </div>
+</template>
